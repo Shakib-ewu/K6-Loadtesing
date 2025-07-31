647ca4d4a26d4a5487c131eebf6c9733ec62c432
Shakib-ewu <hossainsakib58@gmail.com>
2025-07-31 13:42:00 +0600

    New test case added

---
+++ network-throttle-test.js
@@ -0,0 +1,29 @@
+import { browser,networkProfiles } from 'k6/browser';
+
+export const options = {
+    scenarios: {
+      ui: {
+        executor: 'shared-iterations',  // Fixed: Use only iterations
+        vus: 1,  // Number of virtual users
+        iterations: 1,  // Fixed: Replace duration with iterations
+        options: {
+          browser: {
+            type: 'chromium',
+          },
+        },
+      },
+    },
+  };
+
+export default async function () {
+    const page = await browser.newPage();
+    page.setViewportSize(
+        { width: 430, height: 812 }  // Set viewport size for mobile testing
+    );
+    const networkProfile = networkProfiles['Slow 3G']; // Use Slow 3G profile
+    await page.goto("https://rubinoshoes.com/", { waitUntil: 'networkidle' });
+
+    // Close the browser after visiting the page
+    await page.close();
+}
+
